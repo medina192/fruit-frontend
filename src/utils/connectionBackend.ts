89327f6c50940d443da6317116a451b1399a014f
Jose Alejandro Diaz Medina <josealejandrodiazmedina@MacBook-Air-de-Jose.local>
2022-10-03 02:07:36 -0500

    comment backend url

--- src/utils/connectionBackend.ts
+++ src/utils/connectionBackend.ts
@@ -3,7 +3,8 @@ import {
 } from "react-native";
 
 //Dev
-//export const backendUrl = Platform.OS === 'ios' ? "http://localhost:3000" : "http://10.0.2.2:3000"
+export const backendUrl = Platform.OS === 'ios' ? "http://localhost:3000" : "http://10.0.2.2:3000"
 
-// Production
-export const backendUrl = "http://3.134.113.195:3000"
+// Production it doesn't works yet, the aws ec2 server is running, but there is a problem
+// with the security configuration 
+//export const backendUrl = "http://3.134.113.195:3000"
