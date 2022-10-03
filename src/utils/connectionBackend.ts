4d0167a0e7e0fe8a160b65f65df2c72c59c6225d
Jose Alejandro Diaz Medina <josealejandrodiazmedina@MacBook-Air-de-Jose.local>
2022-10-03 01:53:59 -0500

    url backend aws added

--- src/utils/connectionBackend.ts
+++ src/utils/connectionBackend.ts
@@ -2,4 +2,8 @@ import {
     Platform
 } from "react-native";
 
-export const backendUrl = Platform.OS === 'ios' ? "http://localhost:3000" : "http://10.0.2.2:3000"
+//Dev
+//export const backendUrl = Platform.OS === 'ios' ? "http://localhost:3000" : "http://10.0.2.2:3000"
+
+// Production
+export const backendUrl = "http://3.134.113.195:3000"
